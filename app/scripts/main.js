9330a3b0898c376666300cc9e7ecb0d243fec194
Matthew Bucci <mbucci@ucmerced.edu>
2014-09-14 11:20:58 -0700

    Navigation Finished

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,51 @@
 $(document).ready(function(){
 
-	$(".menu-toggle").click(function(){
 
-		if($(".menu").css("display") === "block") {
-			$(".menu").css("display","none");
-		} else {
-			$(".menu").css("display","block");
-		}
-		
-	})
+	// Menu
 
-	$(window).scroll(function (event) {
-	    var scroll = $(window).scrollTop();
-	    if(scroll > $(".slider").outerHeight()) {
-	    	$("header").css("position","fixed");
-	    	$(".slider").css("margin-bottom","100px");
-	    } else {
-	    	$("header").css("position","relative");
-	    	$(".slider").css("margin-bottom","0px");
-	    }
-	});
+		// Responsive
+			
+			$(".menu-toggle").click(function(){
 
-	$(window).resize(function(){
-		if($(window).width() > 1290) {
-			$(".menu").css("display","block");
-		} else {
-			$(".menu").css("display","none");
-		}
-	});
+				if($(".menu").css("display") === "block") {
+					$(".menu").css("display","none");
+				} else {
+					$(".menu").css("display","block");
+				}
+				
+			})
+
+			$(window).scroll(function (event) {
+			    var scroll = $(window).scrollTop();
+			    if(scroll > $(".slider").outerHeight()) {
+			    	$("header").css("position","fixed");
+			    	$(".slider").css("margin-bottom","100px");
+			    } else {
+			    	$("header").css("position","relative");
+			    	$(".slider").css("margin-bottom","0px");
+			    }
+			});
+
+			$(window).resize(function(){
+				if($(window).width() > 1290) {
+					$(".menu").css("display","block");
+				} else {
+					$(".menu").css("display","none");
+				}
+			});
+
+		// Smooth Scroll
+
+		$('a[href^="#"]').on('click',function (e) {
+		    e.preventDefault();
+
+		    var target = this.hash,
+		    $target = $(target);
+
+		    $('html, body').stop().animate({
+		        'scrollTop': $target.offset().top - 150
+		    }, 900, 'swing', function () {
+		        window.location.hash = target;
+		    });
+		});
 })
