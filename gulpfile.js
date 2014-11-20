a66c50ba16ed40b6908f8da9051646346de04f31
Matthew Bucci <buccim2@gene.com>
2014-11-19 21:34:58 -0800

    restructured website to use gulp templates and scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,100 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-
-// Static server
-gulp.task('serve', function() {
-    browserSync({
-        server: {
-            baseDir: "./website/"
-        }
-    });
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    rubysass = require('gulp-ruby-sass'),
+    fileinclude = require('gulp-file-include'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    livereload = require('gulp-livereload'),
+    lr = require('tiny-lr'),
+    connect = require('gulp-connect'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    server = lr(),
+    path = require("path");
+ 
+var paths = {
+  templates: './src/website',
+  sass: './src/scss/'
+};
+ 
+// fileinclude: grab partials from templates and render out html files
+// ==========================================
+gulp.task('fileinclude', function() {
+  return  gulp.src(path.join(paths.templates, '*.tpl.html'))
+    .pipe(fileinclude())
+    .pipe(rename({
+      extname: ""
+     }))
+    .pipe(rename({
+      extname: ".html"
+     }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(livereload(server))
+    .pipe(notify({ message: 'Includes: included' }));
+});
+ 
+//  Sass: compile sass to css task - uses Libsass
+//===========================================
+gulp.task('sass', function() {
+  return gulp.src(path.join(paths.sass, '*.scss'))
+    .pipe(sass({ style: 'expanded', sourceComments: 'map', errLogToConsole: true}))
+    .pipe(autoprefixer('last 2 version', "> 1%", 'ie 8', 'ie 9'))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(livereload(server))
+    .pipe(notify({ message: 'LibSass files dropped!' }));
+});
+ 
+// Assets: copy any remaining assets like JS that we want
+//===========================================
+gulp.task('copy', function(){
+  gulp.src('./src/website/assets/**')
+    .pipe(gulp.dest('dist/assets'));
 });
 
-gulp.task('bs-reload', function () {
-    browserSync.reload();
+ 
+//  Connect: sever task
+//===========================================
+gulp.task('connect', function() {
+  connect.server({
+    port: 9000,
+    root: [__dirname] + "/dist/",
+    livereload: false
+  });
 });
+ 
+function watchStuff(task) {
+  // Listen on port 35729
+  server.listen(35729, function (err) {
+    if (err) {
+      return console.error(err) 
+      //TODO use notify to log a message on Sass compile fail and Beep
+    };
+ 
+    //Watch task for sass
+    gulp.watch(path.join(paths.sass, '**/*.scss'), [task]);
+ 
+    // watch task for gulp-includes
+    gulp.watch(path.join(paths.templates, '**/*.html'), ['fileinclude']);
 
-gulp.task('default', ['serve'], function () {
-    gulp.watch("website/*.html", ['bs-reload']);
-    gulp.watch("website/css/*.css", ['bs-reload']);
-    gulp.watch("website/js/*.js", ['bs-reload']);
+    //Watch task for gulp
+    gulp.watch('gulpfile.js', [task]);
+ 
+  });
+}
+ 
+//  Watch and Livereload using Libsass
+//===========================================
+gulp.task('watch', function() {
+ 
+ watchStuff('sass');
+ 
 });
+ 
+ 
+//  Default Gulp Task
+//===========================================
+gulp.task('default', ['fileinclude', 'sass', 'connect', 'watch'], function() {
+ 
+});
+ 
